8725f7756e213547cd7471439331bfcadbb4d93b
Nydrath <mark.t.dewl@gmail.com>
2017-02-23 20:42:53 +0100

    Added basic Link class.

---
+++ link.py
@@ -0,0 +1,31 @@
+
+from ziruph import Ziruphtable
+
+class MagicalLink:
+    def __init__(self, link):
+        self.link = link
+
+    def __add__(self, other):
+        # Send the smaller link to the big one
+        origin = max(self, other, key=lambda x: len(x.link)).link
+        addon = min(self, other, key=lambda x: len(x.link)).link
+
+        multiplexer = Ziruphtable([c for c in addon])
+        result = ""
+        for idx, c in enumerate(origin):
+            result += multiplexer[c.lower(), addon[idx%len(addon)]]
+
+        return MagicalLink(result)
+
+    def __str__(self):
+        return "Link[{0}]".format(self.link)
+
+    def sendto(self, target):
+        runes = Ziruphtable("Fa Ur Dorn Os Rit Ka Ken Hagal Not Is Jera Ar Sig Tyr Bar Man Yr Eh Vor Gar".split())
+        carrier = MagicalLink(runes[runes[runes[self.link, "Rit"], "Vor"], "Dorn"])
+        return carrier+target
+
+A = MagicalLink("ABC")
+B = MagicalLink("GHJGH")
+C = MagicalLink("Target")
+print((A+B).sendto(C))
