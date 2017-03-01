67505676cd4e441b0970042be8234ea23625609b
Nydrath <mark.t.dewl@gmail.com>
2017-03-01 01:44:13 +0100

    Added a way to specify order of concatenation.

--- link.py
+++ link.py
@@ -7,14 +7,15 @@ class Link:
 
     def __add__(self, other):
         # Send the smaller link to the big one
-        origin = max(self, other, key=lambda x: len(x.link)).link
-        addon = min(self, other, key=lambda x: len(x.link)).link
+        origin = max(self, other, key=lambda x: len(x.link))
+        addon = min(self, other, key=lambda x: len(x.link))
+        return Link(origin.mixwith(addon).link)
 
-        multiplexer = Ziruphtable([c for c in addon])
+    def mixwith(self, other):
+        multiplexer = Ziruphtable([c for c in other.link])
         result = ""
-        for idx, c in enumerate(origin):
-            result += multiplexer[c, addon[idx%len(addon)]]
-
+        for idx, c in enumerate(self.link):
+            result += multiplexer[c, other.link[idx%len(other.link)]]
         return Link(result)
 
     def __str__(self):
