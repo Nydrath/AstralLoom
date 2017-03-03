474d1222c7e6ebb5695e5ee607862f5ed6475911
Nydrath <mark.t.dewl@gmail.com>
2017-03-03 01:00:28 +0100

    Added other method to create sigils.

--- sigilize.py
+++ sigilize.py
@@ -17,3 +17,19 @@ def with_square(sigil):
     draw.rectangle([(0, 0), im.size], fill=255)
     draw.line(pts, fill=0)
     im.save(sigil+".png")
+
+def with_coords(sigil):
+    if isinstance(sigil, Link):
+        sigil = sigil.link
+    border = 40
+    distance = 20
+    im = Image.new("L", (distance*26+border*2, distance*26+border*2))
+    pts = []
+    pairs = [sigil[i:i+2] for i in range(0, len(sigil), 2)]
+    for pair in pairs:
+        if pair.isalpha() and len(pair) == 2:
+            pts.append(tuple(border+distance*(ord(c.lower())-ord('a')) for c in pair))
+    draw = ImageDraw.Draw(im)
+    draw.rectangle([(0, 0), im.size], fill=255)
+    draw.line(pts, fill=0)
+    im.save(sigil+".png")
