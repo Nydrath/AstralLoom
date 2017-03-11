d29959cb507a4a23ad193685b03f756b26dceeec
Nydrath <mark.t.dewl@gmail.com>
2017-03-11 13:59:46 +0100

    Work on sigil generation.

--- magigen.py
+++ magigen.py
@@ -1,20 +1,30 @@
-from PIL import Image, ImageDraw, ImageFont, ImageChops
+from PIL import Image, ImageDraw, ImageFont, ImageChops, ImageFilter
 import numpy as np
 from math import floor, ceil
 import string
 import random
 
-IMAGE_WIDTH = 300
+IMAGE_WIDTH = 600
 IMAGE_HEIGHT = IMAGE_WIDTH
 IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_HEIGHT)
-FONT_SIZE = 100
+FONT_SIZE = 300
 
-full_link = "AJSNRK"
-links = [full_link[i:i+3] for i in range(0, len(full_link), 3)]
-fonts = [ImageFont.truetype("fonts/magi.ttf", size=FONT_SIZE), ImageFont.truetype("fonts/malachim.ttf", size=FONT_SIZE), ImageFont.truetype("fonts/enochian.ttf", size=FONT_SIZE)]
+LINE_LENGTH = 7
+BORDER_WIDTH = int(0.6 * FONT_SIZE)
+LINE_SPACING = int(1.5 * FONT_SIZE)
+
+linksize = 3
+full_link = "".join(["".join(random.sample(string.ascii_uppercase, linksize)) for i in range(49)])
+links = [full_link[i:i+linksize] for i in range(0, len(full_link), linksize)]
+fonts = [
+    ImageFont.truetype("fonts/magi.ttf", size=FONT_SIZE),
+    ImageFont.truetype("fonts/malachim.ttf", size=FONT_SIZE),
+    ImageFont.truetype("fonts/enochian.ttf", size=FONT_SIZE)
+]
+
 nrotations = 8
 rotations = [int(i*360/nrotations) for i in range(nrotations)]
-sigils = []
+sigils = [[] for i in range(LINE_LENGTH)]
 for idx1,link in enumerate(links):
     im = Image.new("L", IMAGE_SIZE)
     imdraw = ImageDraw.Draw(im)
@@ -46,10 +56,22 @@ for idx1,link in enumerate(links):
         t = Image.new("L", IMAGE_SIZE)
         t.paste(tmp, (xoptimal, yoptimal))
         im = ImageChops.lighter(im, t)
-    im.save(str(idx1)+".png")
+    im = im.filter(ImageFilter.MaxFilter(5))
+    im = im.filter(ImageFilter.MinFilter(5))
+    # Removes gray squares but also destroys the anti-aliasing
+    #im = Image.eval(im, lambda x: 255*(x==255))
+    sigils[int(idx1/LINE_LENGTH)].append(im.crop(im.getbbox()))
 
-'''
-    sigils.append(im.crop(im.getbbox()))
+maxwidth = 0
+for line in sigils:
+    for im in line:
+        maxwidth = max(im.size[0], maxwidth)
 
-for idx,sigil in enumerate(sigils):
-    sigil.save(str(idx)+".png")'''
+baseim = Image.new("L", (maxwidth*LINE_LENGTH + 2*BORDER_WIDTH, len(sigils)*LINE_SPACING + 2*BORDER_WIDTH))
+for idx1,line in enumerate(sigils):
+    for idx2,im in enumerate(line):
+        t = Image.new("L", baseim.size)
+        t.paste(im, (BORDER_WIDTH + maxwidth*idx2, BORDER_WIDTH + LINE_SPACING*idx1))
+        baseim = ImageChops.lighter(baseim, t)
+
+baseim.save("out.png")
